1c787d0a708ab11216f5763ba4194978ac5a6ad3
Artur_Kazlouski <Artur_Kazlouski@epam.com>
2018-01-29 16:41:29 +0300

    implemented CRUD operation

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+
+const arcticles = require('./articles.json');
+
 const arcticle = {
   author: 'Chaim Gartenberg',
   description:
@@ -11,7 +14,7 @@ const arcticle = {
   url:
     'https://www.theverge.com/2018/1/27/16936922/pebble-smartwatch-hardware-button-physical-controls',
   urlToImage:
-    'https://cdn.vox-cdn.com/thumbor/pd3WIOBrDA20f0Tg9T9SZBwm_P8=/0x76:1100x652/fit-in/1200x630/cdn.vox-cdn.com/assets/2001833/DSC_0164-2.jpg',
+    'https://cdn.vox-cdn.com/thumbor/pd3WIOBrDA20f0Tg9T9SZBwm_P8=/0x76:1100x652/fit-in/1200x630/cdn.vox-cdn.com/assets/2001833/DSC_0164-2.jpg'
 };
 
 app.listen(3000, function() {
@@ -19,12 +22,32 @@ app.listen(3000, function() {
 });
 
 router.use(function(req, res, next) {
-  res.json(arcticle);
+  res.json(arcticles);
   next();
 });
 
-// app.get('/', function(req, res) {
-//   res.send('Hello, world!');
-// });
+app.get('/articles', function(req, res) {
+  res.json(arcticles);
+});
+
+app.get('/articles/:id', function(req, res, next) {
+  res.json(arcticles.filter(item => item.source.id === req.params.id));
+  next();
+});
+
+app.post('/articles', function(req, res) {
+  res.send(arcticle);
+});
+
+app.put('/articles/:id', function(req, res) {
+  res.send({
+    description:
+      'Looking back at the hardware legacy of the Pebble with its pending Fitbit shutdown as smartwatches shift to touchscreens'
+  });
+});
+
+app.delete('/articles/:id', function(req, res) {
+  res.send([]);
+});
 
 app.use('/', router);
